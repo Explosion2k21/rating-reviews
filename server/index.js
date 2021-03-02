77876c541be05b6f05180b237cf3c4e41cd09765
ZribiHaythem <zribihaythem@gmail.com>
2021-03-02 16:55:22 +0100

    more-reviews-button

--- server/index.js
+++ server/index.js
@@ -5,13 +5,14 @@ const path = require("path");
 const app = express();
 const { routerRating } = require("./routes/routerRating.js");
 const { routerRatingBar } = require("./routes/routerRatingBar.js");
+const { routerReviews } = require("./routes/routerReviews.js");
 const port = process.env.PORT;
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/rating", routerRating);
 app.use("/ratingBar", routerRatingBar);
-app.use("/reviews", routerRating);
+app.use("/reviews", routerReviews);
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
