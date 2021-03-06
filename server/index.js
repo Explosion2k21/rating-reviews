23c8cd33949326253eab75e2906cc8e7d1c0af03
ZribiHaythem <zribihaythem@gmail.com>
2021-03-06 11:40:39 +0100

    change proccess envirement

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const path = require("path");
 const app = express();
 var cors = require("cors");
-const TOKEN =  process.env.TOKEN || "38191b79b05a24118be0ff19f7b4797a6c14b6c4";
+const TOKEN = process.env.TOKEN || "38191b79b05a24118be0ff19f7b4797a6c14b6c4";
 
 const port = process.env.PORT || 3004;
 
@@ -19,5 +19,5 @@ app.use("/reviews/ratingBar", routerRatingBar);
 app.use("/reviews", routerReviews);
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`server running at: http://${process.env.DEV_HOST}:${port}`);
 });
